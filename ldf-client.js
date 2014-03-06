32d5cb20337a8e42a559b1d54a48def5ff0d6a5d
Ruben Verborgh <ruben@verborgh.org>
2014-03-06 20:23:51 +0000

    Export main module.

---
+++ ldf-client.js
@@ -0,0 +1,8 @@
+/*! @license ©2013 Ruben Verborgh - Multimedia Lab / iMinds / Ghent University */
+/** Main ldf-client module exports. */
+
+module.exports = {
+  LinkedDataFragmentsClient: require('./lib/LinkedDataFragmentsClient'),
+  SparqlQuerySolver: require('./lib/SparqlQuerySolver'),
+  Logger: require('./lib/Logger'),
+};
