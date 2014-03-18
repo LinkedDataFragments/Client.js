4471dbae4ea219358908afe585e467a13462041f
Ruben Verborgh <ruben@verborgh.org>
2014-03-18 15:46:51 +0000

    FragmentsClient should parse the fragment.

--- test/http/FragmentsClient-test.js
+++ test/http/FragmentsClient-test.js
@@ -2,7 +2,8 @@
 var FragmentsClient = require('../../lib/http/FragmentsClient');
 
 var UriTemplate = require('uritemplate'),
-    rdf = require('../../lib/rdf/RdfUtil');
+    rdf = require('../../lib/rdf/RdfUtil'),
+    fs = require('fs');
 
 describe('FragmentsClient', function () {
   describe('The FragmentsClient module', function () {
@@ -20,22 +21,39 @@ describe('FragmentsClient', function () {
   });
 
   describe('A FragmentsClient with a URI template', function () {
-    var fragment = 'fragment',
-        httpClient = { get: sinon.stub().returns(fragment) },
-        template = UriTemplate.parse('http://example.org/dataset{?subject,predicate,object}');
+    var template = UriTemplate.parse('http://example.org/dataset{?subject,predicate,object}'),
+        httpClient = {};
     var client = new FragmentsClient(template, { httpClient: httpClient });
 
     describe('when asked for ?s ?o York', function () {
       var pattern = rdf.triple('?s', '?o', rdf.DBPEDIA + 'York');
-      var result = client.getFragmentByPattern(pattern);
 
-      it('should GET the corresponding fragment', function () {
-        httpClient.get.should.have.been.calledOnce;
-        httpClient.get.should.have.been.calledWith('http://example.org/dataset?object=http%3A%2F%2Fdbpedia.org%2Fresource%2FYork');
+      describe('and receiving a Turtle response', function () {
+        var fragment = fs.createReadStream(__dirname + '/../data/fragments/$-birthplace-york.ttl');
+        httpClient.get = sinon.stub().returns(fragment);
+        var result = client.getFragmentByPattern(pattern);
+        fragment.emit('contentType', 'text/turtle');
+
+        it('should GET the corresponding fragment', function () {
+          httpClient.get.should.have.been.calledOnce;
+          httpClient.get.should.have.been.calledWith('http://example.org/dataset?object=http%3A%2F%2Fdbpedia.org%2Fresource%2FYork');
+        });
+
+        it('should stream the triples in the fragment', function (done) {
+          result.should.be.a.streamWithLength(39, done);
+        });
       });
 
-      it('should return the corresponding fragment', function () {
-        result.should.equal(fragment);
+      describe('and receiving a non-supported response', function () {
+        var fragment = fs.createReadStream(__filename);
+        httpClient.get = sinon.stub().returns(fragment);
+        var result = client.getFragmentByPattern(pattern), resultError;
+        result.on('error', function (error) { resultError = error; });
+        fragment.emit('contentType', 'application/javascript');
+
+        it('should emit an error', function () {
+          resultError.should.deep.equal(new Error('No parser for application/javascript'));
+        });
       });
     });
   });
