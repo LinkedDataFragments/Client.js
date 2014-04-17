77d530ff5a80b1b0435ddce88098e43d95bb597a
Ruben Verborgh <ruben@verborgh.org>
2014-04-17 22:47:54 +0200

    FragmentsClient supports multi-page fragments.

--- test/http/FragmentsClient-test.js
+++ test/http/FragmentsClient-test.js
@@ -25,8 +25,10 @@ describe('FragmentsClient', function () {
     startFragment.setProperty('controls', {
       getFragmentUrl: function (pattern) {
         var encode = encodeURIComponent;
-        return 'http://fragment/?s=' + encode(pattern.subject || '') +
-               '&p=' + encode(pattern.predicate || '') + '&o=' + encode(pattern.object || '');
+        return 'http://data.linkeddatafragments.org/dbpedia' +
+               '?subject='   + encode(pattern.subject || '') +
+               '&predicate=' + encode(pattern.predicate || '') +
+               '&object='    + encode(pattern.object || '');
       },
     });
     function createClient(httpClient) {
@@ -36,7 +38,7 @@ describe('FragmentsClient', function () {
       return { get: sinon.stub().returns(fragment) };
     }
 
-    describe('when asked for a pattern', function () {
+    describe('when asked for ?s ?o dbpedia:York', function () {
       var pattern = rdf.triple('?s', '?o', rdf.DBPEDIA + 'York');
 
       describe('and receiving a Turtle response', function () {
@@ -48,7 +50,7 @@ describe('FragmentsClient', function () {
 
         it('should GET the corresponding fragment', function () {
           httpClient.get.should.have.been.calledOnce;
-          httpClient.get.should.have.been.calledWith('http://fragment/?s=&p=&o=http%3A%2F%2Fdbpedia.org%2Fresource%2FYork');
+          httpClient.get.should.have.been.calledWith('http://data.linkeddatafragments.org/dbpedia?subject=&predicate=&object=http%3A%2F%2Fdbpedia.org%2Fresource%2FYork');
         });
 
         it('should stream the triples in the fragment', function (done) {
@@ -66,6 +68,43 @@ describe('FragmentsClient', function () {
 
         it('should emit an error', function () {
           resultError.should.deep.equal(new Error('No parser for application/javascript'));
+        });
+      });
+    });
+
+    describe('when asked for ?p a dbpedia:Artist', function () {
+      var pattern = rdf.triple('?p', rdf.RDF_TYPE, rdf.DBPEDIA + 'Artist');
+
+      describe('and receiving a multi-page response', function () {
+        // Initialize pages
+        var page1 = Iterator.fromStream(fs.createReadStream(__dirname + '/../data/fragments/$-type-artist.ttl'));
+        var page2 = Iterator.fromStream(fs.createReadStream(__dirname + '/../data/fragments/$-type-artist-page2.ttl'));
+        var page3 = Iterator.empty();
+        // Set content types
+        page1.setProperty('contentType', 'text/turtle');
+        page2.setProperty('contentType', 'text/turtle');
+        page3.setProperty('contentType', 'text/turtle');
+        // Stub HTTP client so it returns the pages
+        var httpClient = { get: sinon.stub() };
+        httpClient.get.onCall(0).returns(page1);
+        httpClient.get.onCall(1).returns(page2);
+        httpClient.get.onCall(2).returns(page3);
+        httpClient.get.onCall(3).throws();
+        // Create fragments client
+        var client = createClient(httpClient);
+        var result = client.getFragmentByPattern(pattern);
+
+        it('should GET the corresponding fragment pages', function () {
+          result.on('end', function () {
+            httpClient.get.should.have.been.calledThrice;
+            httpClient.get.getCall(0).args[0].should.equal('http://data.linkeddatafragments.org/dbpedia?subject=&predicate=http%3A%2F%2Fwww.w3.org%2F1999%2F02%2F22-rdf-syntax-ns%23type&object=http%3A%2F%2Fdbpedia.org%2Fresource%2FArtist');
+            httpClient.get.getCall(1).args[0].should.equal('http://data.linkeddatafragments.org/dbpedia?subject=&predicate=rdf%3Atype&object=dbpedia-owl%3AArtist&page=2');
+            httpClient.get.getCall(2).args[0].should.equal('http://data.linkeddatafragments.org/dbpedia?subject=&predicate=rdf%3Atype&object=dbpedia-owl%3AArtist&page=3');
+          });
+        });
+
+        it('should stream the triples in all pages of the fragment', function (done) {
+          result.should.be.a.iteratorWithLength(87, done);
         });
       });
     });
