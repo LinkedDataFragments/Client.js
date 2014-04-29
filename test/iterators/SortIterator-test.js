aa0b488fd49ec889fdc2a1ea53cdafd4ca25670e
Ruben Verborgh <ruben@verborgh.org>
2014-04-29 21:18:11 +0200

    Implement SortIterator with binary search.

--- test/iterators/SortIterator-test.js
+++ test/iterators/SortIterator-test.js
@@ -33,103 +33,32 @@ describe('SortIterator', function () {
 
   describe('A SortIterator having an infinite window length', function () {
     describe('with a single item', function () {
-      var iterator = new SortIterator(Iterator.single(1)), endEmitted = 0;
-      iterator.on('end', function () { endEmitted++; });
-
-      it('should return the item', function () {
-        expect(iterator.read()).to.equal(1);
-      });
-
-      it('should not read more items', function () {
-        expect(iterator.read()).to.equal(null);
-      });
-
-      it('should have ended', function () {
-        iterator.ended.should.be.true;
-      });
-
-      it('should have emitted end', function () {
-        endEmitted.should.equal(1);
+      var iterator = new SortIterator(Iterator.single(1));
+      it('should return the item', function (done) {
+        iterator.should.be.an.iteratorOf([1], done);
       });
     });
 
     describe('with 8 items', function () {
-      var iterator = new SortIterator(Iterator.fromArray([8, 6, 4, 3, 1, 2, 7, 5])), endEmitted = 0;
-      iterator.on('end', function () { endEmitted++; });
-
-      it('should return the sorted items', function () {
-        expect(iterator.read()).to.equal(1);
-        expect(iterator.read()).to.equal(2);
-        expect(iterator.read()).to.equal(3);
-        expect(iterator.read()).to.equal(4);
-        expect(iterator.read()).to.equal(5);
-        expect(iterator.read()).to.equal(6);
-        expect(iterator.read()).to.equal(7);
-        expect(iterator.read()).to.equal(8);
-      });
-
-      it('should not read more items', function () {
-        expect(iterator.read()).to.equal(null);
-      });
-
-      it('should have ended', function () {
-        iterator.ended.should.be.true;
-      });
-
-      it('should have emitted end', function () {
-        endEmitted.should.equal(1);
+      var iterator = new SortIterator(Iterator.fromArray([8, 6, 4, 3, 1, 2, 7, 5]));
+      it('should return the sorted items', function (done) {
+        iterator.should.be.an.iteratorOf([1, 2, 3, 4, 5, 6, 7, 8], done);
       });
     });
   });
 
   describe('A SortIterator having window length 4', function () {
     describe('with a single item', function () {
-      var iterator = new SortIterator(Iterator.single(1), { window: 4 }), endEmitted = 0;
-      iterator.on('end', function () { endEmitted++; });
-
-      it('should return the item', function () {
-        expect(iterator.read()).to.equal(1);
-      });
-
-      it('should not read more items', function () {
-        expect(iterator.read()).to.equal(null);
-      });
-
-      it('should have ended', function () {
-        iterator.ended.should.be.true;
-      });
-
-      it('should have emitted end', function () {
-        endEmitted.should.equal(1);
+      var iterator = new SortIterator(Iterator.single(1), { window: 4 });
+      it('should return the item', function (done) {
+        iterator.should.be.an.iteratorOf([1], done);
       });
     });
 
     describe('with 8 items', function () {
-      var iterator = new SortIterator(Iterator.fromArray([8, 6, 4, 3, 1, 2, 7, 5]), { window: 4 }),
-          endEmitted = 0;
-      iterator.on('end', function () { endEmitted++; });
-
-      it('should return the sorted items', function () {
-        expect(iterator.read()).to.equal(3);
-        expect(iterator.read()).to.equal(1);
-        expect(iterator.read()).to.equal(2);
-        expect(iterator.read()).to.equal(4);
-        expect(iterator.read()).to.equal(5);
-        expect(iterator.read()).to.equal(6);
-        expect(iterator.read()).to.equal(7);
-        expect(iterator.read()).to.equal(8);
-      });
-
-      it('should not read more items', function () {
-        expect(iterator.read()).to.equal(null);
-      });
-
-      it('should have ended', function () {
-        iterator.ended.should.be.true;
-      });
-
-      it('should have emitted end', function () {
-        endEmitted.should.equal(1);
+      var iterator = new SortIterator(Iterator.fromArray([8, 6, 4, 3, 1, 2, 7, 5]), { window: 4 });
+      it('should return the items, sorted with a lookahead of 4', function (done) {
+        iterator.should.be.an.iteratorOf([3, 1, 2, 4, 5, 6, 7, 8], done);
       });
     });
   });
@@ -138,52 +67,16 @@ describe('SortIterator', function () {
     function reverseSort(a, b) { return b - a; }
 
     describe('with a single item', function () {
-      var iterator = new SortIterator(Iterator.single(1), reverseSort), endEmitted = 0;
-      iterator.on('end', function () { endEmitted++; });
-
-      it('should return the item', function () {
-        expect(iterator.read()).to.equal(1);
-      });
-
-      it('should not read more items', function () {
-        expect(iterator.read()).to.equal(null);
-      });
-
-      it('should have ended', function () {
-        iterator.ended.should.be.true;
-      });
-
-      it('should have emitted end', function () {
-        endEmitted.should.equal(1);
+      var iterator = new SortIterator(Iterator.single(1), reverseSort);
+      it('should return the item', function (done) {
+        iterator.should.be.an.iteratorOf([1], done);
       });
     });
 
     describe('with 8 items', function () {
-      var iterator = new SortIterator(Iterator.fromArray([8, 6, 4, 3, 1, 2, 7, 5]), reverseSort),
-          endEmitted = 0;
-      iterator.on('end', function () { endEmitted++; });
-
-      it('should return the sorted items', function () {
-        expect(iterator.read()).to.equal(8);
-        expect(iterator.read()).to.equal(7);
-        expect(iterator.read()).to.equal(6);
-        expect(iterator.read()).to.equal(5);
-        expect(iterator.read()).to.equal(4);
-        expect(iterator.read()).to.equal(3);
-        expect(iterator.read()).to.equal(2);
-        expect(iterator.read()).to.equal(1);
-      });
-
-      it('should not read more items', function () {
-        expect(iterator.read()).to.equal(null);
-      });
-
-      it('should have ended', function () {
-        iterator.ended.should.be.true;
-      });
-
-      it('should have emitted end', function () {
-        endEmitted.should.equal(1);
+      var iterator = new SortIterator(Iterator.fromArray([8, 6, 4, 3, 1, 2, 7, 5]), reverseSort);
+      it('should return the sorted items', function (done) {
+        iterator.should.be.an.iteratorOf([8, 7, 6, 5, 4, 3, 2, 1], done);
       });
     });
   });
