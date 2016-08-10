LinkedDataFragments/Client.js

commit 964bbd3ca10e4add6647c152003ebde92789ab84

Port writers.

lib/writers/SparqlResultWriter.js
@@ -1,34 +1,18 @@
 /*! @license ©2014 Miel Vander Sande - Multimedia Lab / iMinds / Ghent University */
 /* Serializing the output of a SparqlIterator */
 
-var TransformIterator = require('../iterators/Iterator').TransformIterator;
+var TransformIterator = require('asynciterator').TransformIterator;
 
-function SparqlResultWriter(mediaType, sparqlIterator) {
-  // Handle subclass construction by initializing an instance
-  if (mediaType === true) {
-    TransformIterator.call(this, true);
-    this._empty = true;
-    return setImmediate((function () {
-      var variables = sparqlIterator.getProperty('variables') || [];
-      this._writeHead(variables.map(function (v) { return v.substring(1); }));
-      this.setSource(sparqlIterator);
-    }).bind(this));
-  }
-  // Otherwise, create a result writer based on the media type
-  else {
-    var ResultWriter = SparqlResultWriter.writers[mediaType];
-    if (!ResultWriter)
-      throw new Error('The requested result format ' + mediaType + ' is not supported.');
-    if (typeof ResultWriter === 'string')
-      ResultWriter = SparqlResultWriter.writers[mediaType] = require(ResultWriter);
-    return new ResultWriter(sparqlIterator);
-  }
+function SparqlResultWriter(source) {
+  TransformIterator.call(this, source);
+  this._empty = true;
+  this._variables = source.getProperty('variables') || [];
 }
-TransformIterator.inherits(SparqlResultWriter);
+TransformIterator.subclass(SparqlResultWriter);
 
-SparqlResultWriter.writers = {};
-SparqlResultWriter.register = function (mimeType, ResultWriter) {
-  SparqlResultWriter.writers[mimeType] = ResultWriter;
+SparqlResultWriter.prototype._begin = function (done) {
+  this._writeHead(this._variables.map(function (v) { return v.substring(1); }));
+  done();
 };
 
 SparqlResultWriter.prototype._writeHead = function (variableNames) { };
@@ -38,16 +22,37 @@ SparqlResultWriter.prototype._transform = function (result, done) {
     this._writeBoolean(result);
   else
     this._writeBindings(result);
-  delete this._empty;
+  this._empty = false;
   done();
 };
 
-SparqlResultWriter.prototype._writeBindings = function (result, done) {
+SparqlResultWriter.prototype._writeBindings = function (result) {
   throw new Error('The _writeBindings method has not been implemented.');
 };
 
-SparqlResultWriter.prototype._writeBoolean = function (result, done) {
+SparqlResultWriter.prototype._writeBoolean = function (result) {
   throw new Error('The _writeBoolean method has not been implemented.');
+};
+
+// Index of registered writers
+var writers = {};
+
+// Register a writer for a given media type
+SparqlResultWriter.register = function (mediaType, ResultWriter) {
+  writers[mediaType] = ResultWriter;
+};
+
+// Instantiate a writer of a given media type
+SparqlResultWriter.instantiate = function (mediaType, source) {
+  // Look up the class or class name
+  var ResultWriter = writers[mediaType];
+  if (!ResultWriter)
+    throw new Error('No writer available for media type ' + mediaType + '.');
+  // If it is a class name, load the class
+  if (typeof ResultWriter === 'string')
+    ResultWriter = writers[mediaType] = require(ResultWriter);
+  // Create an instance of the subclass
+  return new ResultWriter(source);
 };
 
 module.exports = SparqlResultWriter;

bin/ldf-client
@@ -33,7 +33,7 @@ if (!(args.q || args.f) && args._.length < 2 || args._.length < 1 || args.h || a
   return process.exit(1);
 }
 
-// Load main libraries (postponed as to here for speed)
+// Load main libraries (postponed to here for speed)
 var fs = require('fs'),
     path = require('path'),
     N3 = require('n3'),
@@ -64,7 +64,7 @@ try {
   // Write JSON representations of the rows or boolean
   case 'ASK':
   case 'SELECT':
-    writer = new ldf.SparqlResultWriter(mimeType, sparqlIterator);
+    writer = new ldf.SparqlResultWriter.instantiate(mimeType, sparqlIterator);
     writer.on('data', function (data) { process.stdout.write(data); });
     break;
   // Write an RDF representation of all results

lib/writers/JSONResultWriter.js
@@ -4,23 +4,26 @@
 var SparqlResultWriter = require('./SparqlResultWriter');
 
 function JSONResultWriter(sparqlIterator) {
-  SparqlResultWriter.call(this, true, sparqlIterator);
+  SparqlResultWriter.call(this, sparqlIterator);
 }
-SparqlResultWriter.inherits(JSONResultWriter);
+SparqlResultWriter.subclass(JSONResultWriter);
 
 JSONResultWriter.prototype._writeHead = function () {
   this._push('[');
 };
 
-JSONResultWriter.prototype._transform = function (result, done) {
-  this._push(this._empty ? (delete this._empty) && '\n' : ',\n');
-  this._push(JSON.stringify(result).trim());
-  done();
+JSONResultWriter.prototype._writeBindings = function (bindings) {
+  this._push(this._empty ? '\n' : ',\n');
+  this._push(JSON.stringify(bindings).trim());
 };
 
-JSONResultWriter.prototype._end = function () {
+JSONResultWriter.prototype._writeBoolean = function (result) {
+  this._push('\n' + result);
+};
+
+JSONResultWriter.prototype._flush = function (done) {
   this._push(this._empty ? ']\n' : '\n]\n');
-  SparqlResultWriter.prototype._end.call(this);
+  done();
 };
 
 module.exports = JSONResultWriter;

lib/writers/SimpleResultWriter.js
@@ -5,13 +5,13 @@ var SparqlResultWriter = require('./SparqlResultWriter'),
     _ = require('lodash');
 
 function SimpleResultWriter(sparqlIterator) {
-  SparqlResultWriter.call(this, true, sparqlIterator);
+  SparqlResultWriter.call(this, sparqlIterator);
 }
-SparqlResultWriter.inherits(SimpleResultWriter);
+SparqlResultWriter.subclass(SimpleResultWriter);
 
-SimpleResultWriter.prototype._writeHead = function (variables) {
-  var maxLength = _.max(_.pluck(variables, 'length'));
-  variables.map(function (v) {
+SimpleResultWriter.prototype._writeHead = function (variableNames) {
+  var maxLength = _.max(_.pluck(variableNames, 'length'));
+  variableNames.map(function (v) {
     this['?' + v] = new Array(maxLength - v.length + 1).join(' ') + v + ': ';
   }, this._paddedNames = {});
 };

lib/writers/SparqlJSONResultWriter.js
@@ -8,9 +8,9 @@ var SparqlResultWriter = require('./SparqlResultWriter'),
 function SparqlJSONResultWriter(sparqlIterator) {
   if (!(this instanceof SparqlJSONResultWriter))
     return new SparqlJSONResultWriter(sparqlIterator);
-  SparqlResultWriter.call(this, true, sparqlIterator);
+  SparqlResultWriter.call(this, sparqlIterator);
 }
-SparqlResultWriter.inherits(SparqlJSONResultWriter);
+SparqlResultWriter.subclass(SparqlJSONResultWriter);
 
 SparqlJSONResultWriter.prototype._writeHead = function (variableNames) {
   var head = {};
@@ -42,9 +42,9 @@ SparqlJSONResultWriter.prototype._writeBoolean = function (result) {
   this._push('"boolean":' + result + '}\n');
 };
 
-SparqlJSONResultWriter.prototype._end = function () {
+SparqlJSONResultWriter.prototype._flush = function (done) {
   this._push(this._empty ? '"results": { "bindings": [] }}\n' : '\n]}}\n');
-  SparqlResultWriter.prototype._end.call(this);
+  done();
 };
 
 module.exports = SparqlJSONResultWriter;

lib/writers/SparqlXMLResultWriter.js
@@ -7,9 +7,9 @@ var SparqlResultWriter = require('./SparqlResultWriter'),
     xml = require('xml');
 
 function SparqlXMLResultWriter(sparqlIterator) {
-  SparqlResultWriter.call(this, true, sparqlIterator);
+  SparqlResultWriter.call(this, sparqlIterator);
 }
-SparqlResultWriter.inherits(SparqlXMLResultWriter);
+SparqlResultWriter.subclass(SparqlXMLResultWriter);
 
 SparqlXMLResultWriter.prototype._writeHead = function (variableNames) {
   var root = this._root = xml.element({ _attr: { xlmns: 'http://www.w3.org/2005/sparql-results#' } }),
@@ -47,10 +47,10 @@ SparqlXMLResultWriter.prototype._writeBoolean = function (result) {
   this._root.push({ boolean: result });
 };
 
-SparqlXMLResultWriter.prototype._end = function () {
+SparqlXMLResultWriter.prototype._flush = function (done) {
   this._results.close();
   this._root.close();
-  SparqlResultWriter.prototype._end.call(this);
+  done();
 };
 
 module.exports = SparqlXMLResultWriter;

lib/writers/StatsResultWriter.js
@@ -5,12 +5,11 @@
 var SparqlResultWriter = require('./SparqlResultWriter');
 
 function StatsResultWriter(sparqlIterator) {
-  SparqlResultWriter.call(this, true, sparqlIterator);
-  // Init stats
+  SparqlResultWriter.call(this, sparqlIterator);
   this._startTime = process.hrtime();
   this._result = 0;
 }
-SparqlResultWriter.inherits(StatsResultWriter);
+SparqlResultWriter.subclass(StatsResultWriter);
 
 
 StatsResultWriter.prototype._writeHead = function () {
@@ -24,10 +23,10 @@ StatsResultWriter.prototype._transform = function (result, done) {
   done();
 };
 
-StatsResultWriter.prototype._end = function () {
+StatsResultWriter.prototype._flush = function (done) {
   var time = process.hrtime(this._startTime);
   this._push(['TOTAL', time[0] * 1000 + (time[1] / 1000000)].join(',') + '\n');
-  SparqlResultWriter.prototype._end.call(this);
+  done();
 };
 
 module.exports = StatsResultWriter;

lib/writers/TableResultWriter.js
@@ -4,16 +4,15 @@
 var SparqlResultWriter = require('./SparqlResultWriter');
 
 function TableResultWriter(sparqlIterator) {
-  SparqlResultWriter.call(this, true, sparqlIterator);
+  SparqlResultWriter.call(this, sparqlIterator);
   this._columnWidth = 50;
   this._padding = repeat(' ', this._columnWidth);
 }
-SparqlResultWriter.inherits(TableResultWriter);
+SparqlResultWriter.subclass(TableResultWriter);
 
 TableResultWriter.prototype._writeHead = function (variableNames) {
   var header = variableNames.map(this._pad, this).join(' ');
   this._push(header +  '\n' + repeat('-', header.length) + '\n');
-  this._variables = variableNames.map(function (v) { return '?' + v; });
 };
 
 TableResultWriter.prototype._writeBindings = function (bindings) {

commit afdcacfd3485e0c1b34c03a4ea6719d5d5be7225

Expose writers.
Fixes regression introduced in 964bbd3ca10e4add6647c152003ebde92789ab84.

lib/writers/SparqlResultWriter.js
@@ -35,22 +35,22 @@ SparqlResultWriter.prototype._writeBoolean = function (result) {
 };
 
 // Index of registered writers
-var writers = {};
+SparqlResultWriter.writers = {};
 
 // Register a writer for a given media type
 SparqlResultWriter.register = function (mediaType, ResultWriter) {
-  writers[mediaType] = ResultWriter;
+  SparqlResultWriter.writers[mediaType] = ResultWriter;
 };
 
 // Instantiate a writer of a given media type
 SparqlResultWriter.instantiate = function (mediaType, source) {
   // Look up the class or class name
-  var ResultWriter = writers[mediaType];
+  var ResultWriter = SparqlResultWriter.writers[mediaType];
   if (!ResultWriter)
     throw new Error('No writer available for media type ' + mediaType + '.');
   // If it is a class name, load the class
   if (typeof ResultWriter === 'string')
-    ResultWriter = writers[mediaType] = require(ResultWriter);
+    ResultWriter = SparqlResultWriter.writers[mediaType] = require(ResultWriter);
   // Create an instance of the subclass
   return new ResultWriter(source);
 };