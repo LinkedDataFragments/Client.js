4fef8a9d973e80420eb7f010ec46d163f3c28dbb
Ruben Verborgh <ruben@verborgh.org>
2016-08-14 11:01:04 -0700

    Correct N3 path.

--- browser.js
+++ browser.js
@@ -1,3 +1,4 @@
 // Make the ldf-client module available as a global browser object
 window.ldf = require('./ldf-client');
-window.N3  = require('./node_modules/n3'); // expose the same N3 version as used in the client
+// Expose the same N3 version as used in the client
+window.N3 = require('n3'); 
