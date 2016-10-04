065a724181ae4b0e76614fd8f5f4ef4feeceda1c
Ruben Verborgh <ruben@verborgh.org>
2016-10-04 23:14:07 +0200

    Expose browser objects on global.
    Browserify assigns this to the right value.

--- browser.js
+++ browser.js
@@ -1,6 +1,3 @@
 /*! @license MIT ©2013-2016 Ruben Verborgh - Ghent University / iMinds */
-
-// Make the ldf-client module available as a global browser object
-window.ldf = require('./ldf-client');
-// Expose the same N3 version as used in the client
-window.N3 = require('n3'); 
+global.ldf = require('./ldf-client');
+global.N3 = require('n3');
